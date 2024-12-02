c6541442f00a0e93af40c9018e5a441fe86638df
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-12-02 08:00:17 -0800

    clean up

--- src/components/searchPage/extractModData.js
+++ src/components/searchPage/extractModData.js
@@ -1,17 +1,10 @@
 import { component } from "@tmetcalfe89/vibrations";
 
-export default component(
-  [".mod-tile-right"],
-  {},
-  (parent, {}, { setState }) => {
-    const nameLink = parent.querySelector(".tile-name a");
-    const modId = nameLink.href.split("/")[5];
-    setState(
-      ["mods", modId, "modTitle"],
-      (p) => p || nameLink.innerText.trim()
-    );
+export default component([".mod-tile"], {}, (parent, {}, { setState }) => {
+  const nameLink = parent.querySelector(".tile-name a");
+  const modId = nameLink.href.split("/")[5];
+  setState(["mods", modId, "modTitle"], (p) => p || nameLink.innerText.trim());
 
-    const descEl = parent.querySelector(".desc");
-    setState(["mods", modId, "notes"], (p) => p || descEl.innerText.trim());
-  }
-);
+  const descEl = parent.querySelector(".desc");
+  setState(["mods", modId, "notes"], (p) => p || descEl.innerText.trim());
+});
--- src/components/searchPage/modManagementTitleSection.js
+++ src/components/searchPage/modManagementTitleSection.js
@@ -1,10 +1,12 @@
 import { component } from "@tmetcalfe89/vibrations";
+import searchPageStyle from "../../style/searchPage.module.css";
 
 export default component(
   [".mod-tile .tile-name"],
   {},
   (parent, {}, { createElement }) => {
     const container = createElement("container", "div");
+    container.className = searchPageStyle.modTileTitleActions;
     container.dataset.target = "mod-management-search-page-mod-tile";
     const modId = parent.querySelector("a").href.split("/")[5];
     container.dataset.modId = modId;
