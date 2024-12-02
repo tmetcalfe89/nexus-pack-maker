274c13e96a5d15758fac3b4475f6f75f39bb3578
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-12-01 22:25:50 -0800

    extract mod data from search page

---
+++ src/components/searchPage/extractModData.js
@@ -0,0 +1,17 @@
+import { component } from "@tmetcalfe89/vibrations";
+
+export default component(
+  [".mod-tile-right"],
+  {},
+  (parent, {}, { setState }) => {
+    const nameLink = parent.querySelector(".tile-name a");
+    const modId = nameLink.href.split("/")[5];
+    setState(
+      ["mods", modId, "modTitle"],
+      (p) => p || nameLink.innerText.trim()
+    );
+
+    const descEl = parent.querySelector(".desc");
+    setState(["mods", modId, "notes"], (p) => p || descEl.innerText.trim());
+  }
+);
--- src/components/searchPage/index.js
+++ src/components/searchPage/index.js
@@ -1,8 +1,10 @@
+import extractModData from "./extractModData";
 import modManagementTitleSection from "./modManagementTitleSection";
 import toggleInPackButton from "./toggleInPackButton";
 import toggleRejectButton from "./toggleRejectButton";
 
 export default [
+  extractModData,
   modManagementTitleSection,
   toggleInPackButton,
   toggleRejectButton,
